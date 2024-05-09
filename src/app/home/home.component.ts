cc74344d66d774fdbb3f7a8dc52e0b70ab86bcc0
Eden-Dahary <edendahary1@gmail.com>
2024-05-09 19:11:00 +0300

    new star rating for players

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,35 +1,97 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { TeamsComponent } from '../teams/teams.component';
 import axios from 'axios';
 import { ParticleAnimationComponent } from "../particle-animation/particle-animation.component";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
+import { faStar as faStarEmpty } from '@fortawesome/free-regular-svg-icons';
+
+
 
 @Component({
-    selector: 'app-home',
-    standalone: true,
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.css',
-    imports: [RouterOutlet, FormsModule, RouterModule, TeamsComponent, ParticleAnimationComponent]
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    RouterModule,
+    TeamsComponent,
+    ParticleAnimationComponent,
+    FontAwesomeModule,
+  ],
 })
 export class HomeComponent implements OnInit {
+  constructor(private changeDetectorRef: ChangeDetectorRef) {}
+
   @ViewChild('myModal') model: ElementRef | undefined;
   @ViewChild('modelError') modelError: ElementRef | undefined;
   PlayerObj: Player = new Player();
   playerList: Player[] = [];
   Message: string = '';
-  // private apiUrl = 'http://localhost:3000/api/';   // localhost 
+  // private apiUrl = 'http://localhost:3000/api/';   // localhost
   private apiUrl = 'https://teammaker-5.onrender.com/api/'; // my server on render.com IP
   // private apiUrl = `https://192.168.1.112:3000/api/`; // my pc IP
   showBuffer: boolean = false;
-
   data: any;
+
+  faStar = faStar;
+  faStarHalfAlt = faStarHalfAlt;
+  faStarEmpty = faStarEmpty;
+
+  showHalfStar = true;
+
+
+
+  getStarIcon(num: number, overallGrade: number) {
+    if (num <= overallGrade) {
+      return this.faStar;
+    } else if (
+      num > overallGrade &&
+      num < overallGrade + 1 &&
+      this.showHalfStar
+    ) {
+      return this.faStarHalfAlt;
+    } else {
+      return this.faStarEmpty;
+    }
+  }
+
+  rateStar(field: string, index: number): void {
+    switch (field) {
+      case 'dribbling':
+        this.PlayerObj.dribbling = index + 1;
+        break;
+      case 'shot':
+        this.PlayerObj.shot = index + 1;
+        break;
+      case 'pace':
+        this.PlayerObj.pace = index + 1;
+        break;
+      case 'pass':
+        this.PlayerObj.pass = index + 1;
+        break;
+      case 'accuracy':
+        this.PlayerObj.accuracy = index + 1;
+        break;
+      case 'attack':
+        this.PlayerObj.attack = index + 1;
+        break;
+      case 'defense':
+        this.PlayerObj.defense = index + 1;
+        break;
+    }
+  }
 
   async ngOnInit() {
     this.showBuffer = true;
     this.data = await this.getPlayers();
     this.showBuffer = false;
   }
+
   async refreshePage() {
     this.showBuffer = true;
 
@@ -102,11 +164,6 @@ export class HomeComponent implements OnInit {
     if (isDelete) {
       this.deletePlayer(item);
     }
-    // if (isDelete) {
-    //   const currentRecord = this.playerList.findIndex((m) => m.id === item.id);
-    //   this.playerList.splice(currentRecord, 1);
-    //   localStorage.setItem('angular17crud', JSON.stringify(this.playerList));
-    // }
   }
   playerTotalAverage() {
     return parseFloat(
@@ -124,8 +181,10 @@ export class HomeComponent implements OnInit {
   }
 
   async savePlayer() {
+    debugger;
     this.showBuffer = true;
     this.PlayerObj.overall_grade = this.playerTotalAverage();
+
     const newPlayer = await this.addPlayer(this.PlayerObj);
     if (newPlayer) {
       alert('New player added');
@@ -139,7 +198,6 @@ export class Player {
   _id: number | undefined;
   name!: string;
   mobile!: string;
-  city!: string;
   shot!: number;
   pass!: number;
   accuracy!: number;
